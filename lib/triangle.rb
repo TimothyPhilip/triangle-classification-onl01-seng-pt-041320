0f6eed86f34cf474b75e957db7d16361629e95dd
Philippe Timothe <philippetimothe@gmail.com>
2020-05-17 18:04:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @side_3 = side_3
   end
   
-  def kind
+  def conditional
     if side_1 == side_2 && side_2 == side_3
       :equilateral
     elsif side_1 == side_2 || side_2 == side_3 || side_3 == side_1
@@ -17,19 +17,18 @@ class Triangle
     end
   end
   
- # def validate_triangle  
- #   if side_1 + side_2 > side_3 || side_2 + side_3 > side_1 || side_3 + side_1 > side_2  && side_1 <= 0 || side_2 <= 0 || side_3 <= 0
-#        begin 
- #         raise TriangleError 
-#        rescue TriangleError => error 
-#          conditional
-#        end
-#    end
- # end 
+  def kind 
+    if side_1 + side_2 < side_3 || side_2 + side_3 > side_1 || side_3 + side_1 > side_2 || side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+        begin 
+          raise TriangleError 
+        rescue TriangleError => error 
+          conditional
+        end
+    end
+  end 
   
   
-  class TriangleError < StandardError
-  end
+  
   
   
   
