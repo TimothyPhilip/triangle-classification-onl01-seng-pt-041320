4d69f4699e92dab9db3970b9a3b2d37098209f36
Philippe Timothe <philippetimothe@gmail.com>
2020-05-17 18:15:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
   end
   
   def kind 
-    if side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1 || side_3 + side_1 <= side_2 && side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+    if side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1 || side_3 + side_1 <= side_2 || side_1 <= 0 || side_2 <= 0 || side_3 <= 0
       raise TriangleError
     else
       conditional
