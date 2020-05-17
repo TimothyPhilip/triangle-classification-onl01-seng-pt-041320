faad93920185915685a33c7a80ea06a17c10d9e8
Philippe Timothe <philippetimothe@gmail.com>
2020-05-17 18:01:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,15 +18,15 @@ class Triangle
     end
   end
   
-  def validate_triangle  
-    if side_1 + side_2 > side_3 || side_2 + side_3 > side_1 || side_3 + side_1 > side_2  && side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+ # def validate_triangle  
+ #   if side_1 + side_2 > side_3 || side_2 + side_3 > side_1 || side_3 + side_1 > side_2  && side_1 <= 0 || side_2 <= 0 || side_3 <= 0
 #        begin 
-          raise TriangleError 
+ #         raise TriangleError 
 #        rescue TriangleError => error 
 #          conditional
 #        end
-    end
-  end 
+#    end
+ # end 
   
   
   class TriangleError < StandardError
